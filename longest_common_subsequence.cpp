e038868cd78dbb1f16ef1ea57a4a4cc8bcc2d1df
ravidelcj <ravi.delcj@gmail.com>
2015-08-26 22:56:16 +0530

    Memory allocation problem
    memory allocation was messing up in lcs
    so just made it a global variable with constant field

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -5,43 +5,43 @@
 #define MAX 1002
 
 using namespace std ;
-
+int lcs[500][500];
 int main ()
 {
-        int lena , lenb , i , j ; 
-        char a[MAX] , b[MAX] ; 
-        
-        cin>> a ; 
-        cin >> b ; 
+        int lena , lenb , i , j ;
+        char a[MAX] , b[MAX] ;
+
+        cin>> a ;
+        cin >> b ;
 
 
-        lena = strlen(a) ; 
+        lena = strlen(a) ;
         lenb = strlen(b) ;
 
-        int lcs[lena][lenb] ;  
+
 
         for (i = 0 ;i<=lena ; i++) {
                 for (j = 0 ; j<=lenb ; j++){
-			
+
                         if (i == 0 || j == 0){
                                 lcs[i][j] = 0 ;
                         }
                         else {
-                      		
-                                cout << lcs[i-1][j] << "  "<< lcs[i][j-1] << endl;
-                                lcs[i][j] = max(lcs[i][j-1] ,lcs[i-1][j]) ; 
-                               
+
+                        //        cout << lcs[i-1][j] << "  "<< lcs[i][j-1] << endl;
+                                lcs[i][j] = max(lcs[i][j-1] ,lcs[i-1][j]) ;
+
 				//cout << a[i-1] <<"  "<< b[j-1] << endl;
                                 if (a[i-1] == b[j-1]){
                                         lcs[i][j] = max (lcs[i-1][j-1] +1 , lcs[i][j]) ;
                                 }
-                                cout << lcs[i][j] << endl ;
-                                       
+                         //       cout << lcs[i][j] << endl ;
+
                         }
                 }
         }
 
 	cout << "ans = " << lcs[lena][lenb] << "\n" ;
 
-return 0 ; 
+return 0 ;
 }
